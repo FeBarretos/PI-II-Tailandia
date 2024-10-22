d26dc13a80688a92e8901edd96f2a778d6b3d2ee
William Takahashi <williamtakahashikeiti@gmail.com>
2024-10-22 12:54:18 -0300

    SOM

---
+++ PI_2_Tainlandia/PI_2_Tainlandia/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/keyboard.h>
+#include <allegro5/mouse.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+#include "tela_inicio.h"
+#include "jogo.h"
+
+int main() {
+    // Inicializar Allegro
+    if (!al_init()) {
+        fprintf(stderr, "Falha ao inicializar Allegro\n");
+        return -1;
+    }
+
+    // Inicializa addons e dispositivos
+    al_init_image_addon();
+    al_init_font_addon();
+    al_init_ttf_addon();
+    al_init_primitives_addon();
+    al_init_acodec_addon();
+    al_install_keyboard();
+    al_install_mouse();
+    al_install_audio();
+    // Criar o display
+    ALLEGRO_DISPLAY* display = al_create_display(960, 540);
+    if (!display) {
+        fprintf(stderr, "Falha ao criar o display\n");
+        return -1;
+    }
+
+    ALLEGRO_EVENT_QUEUE* event_queue = al_create_event_queue();
+    al_register_event_source(event_queue, al_get_display_event_source(display));
+    al_register_event_source(event_queue, al_get_keyboard_event_source());
+    al_register_event_source(event_queue, al_get_mouse_event_source());
+
+    // Executar tela de início
+    if (!tela_inicio(event_queue)) {
+        return 0; // Fechar o programa se o usuário sair da tela de início
+    }
+
+    // Executar o jogo
+    jogo(display, event_queue);
+
+    // Finalização
+    al_destroy_display(display);
+    al_destroy_event_queue(event_queue);
+    return 0;
+}
---
+++ PI_2_Tainlandia/PI_2_Tainlandia/tela_inicio.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+#include <allegro5/allegro_acodec.h>
+#include "tela_inicio.h"
+#include "configuracoes.h"
+
+
+
+
+// Função para verificar se o mouse está sobre um botão
+bool mouse_sobre_botao(int mouse_x, int mouse_y, int botao_x, int botao_y, int largura_botao, int altura_botao) {
+    return (mouse_x >= botao_x && mouse_x <= botao_x + largura_botao &&
+        mouse_y >= botao_y && mouse_y <= botao_y + altura_botao);
+}
+
+bool tela_inicio(ALLEGRO_EVENT_QUEUE* event_queue) {
+    // Carregar a imagem de fundo
+    ALLEGRO_BITMAP* fundo = al_load_bitmap("tela_de_inicio.png");
+    if (!fundo) {
+        fprintf(stderr, "Falha ao carregar a imagem de fundo\n");
+        return false;
+    }
+    //Carrega efeito sonoro
+    al_reserve_samples(1);
+    ALLEGRO_SAMPLE* music = al_load_sample("music.ogg");
+    ALLEGRO_SAMPLE_INSTANCE* songInst = al_create_sample_instance(music);
+    al_set_sample_instance_playmode(songInst, ALLEGRO_PLAYMODE_LOOP);
+    al_attach_sample_instance_to_mixer(songInst, al_get_default_mixer());
+    
+    // Largura e altura dos botões (ajustar conforme necessário)
+    int largura_botao = 100, altura_botao = 40;
+
+    // Coordenadas dos botões na imagem
+    int botao_play_x = 415, botao_play_y = 190; // Coordenadas do botão "Play"
+    int botao_settings_x = 415, botao_settings_y = 260; // Coordenadas do botão "Settings"
+    int botao_quit_x = 415, botao_quit_y = 330; // Coordenadas do botão "Quit"
+    bool Pmusic = true;
+    bool iniciar_jogo = false;
+    bool rodando = true;
+
+    
+    al_play_sample_instance(songInst);
+    
+
+    while (rodando) {
+        ALLEGRO_EVENT ev;
+        al_wait_for_event(event_queue, &ev);
+       
+        if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+            al_destroy_bitmap(fundo); // Destruir o bitmap ao fechar o jogo
+            al_destroy_sample_instance(songInst); //destruir musica
+            al_destroy_sample(music); // destruir musica
+
+            return false;
+        }
+        else if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
+            int mouse_x = ev.mouse.x;
+            int mouse_y = ev.mouse.y;
+
+            // Verificar se o clique foi sobre o botão "Play"
+            if (mouse_sobre_botao(mouse_x, mouse_y, botao_play_x, botao_play_y, largura_botao, altura_botao)) {
+                iniciar_jogo = true;
+                rodando = false;  // Para sair do loop e iniciar o jogo
+            }
+            // Verificar se o clique foi sobre o botão "Settings"
+            else if (mouse_sobre_botao(mouse_x, mouse_y, botao_settings_x, botao_settings_y, largura_botao, altura_botao)) {
+                configuracoes(event_queue);  // Abre o menu de configurações
+
+                Pmusic = false; // para mutar a musica
+               
+            }
+            // Verificar se o clique foi sobre o botão "Quit"
+            else if (mouse_sobre_botao(mouse_x, mouse_y, botao_quit_x, botao_quit_y, largura_botao, altura_botao)) {
+                rodando = false;  // Fecha o programa
+            }
+        }
+
+        // Desenhar a tela de início com a imagem de fundo
+        al_draw_bitmap(fundo, 0, 0, 0);  // Desenhar a imagem de fundo na posição (0, 0)
+        al_flip_display();
+    }
+
+    al_destroy_sample(music);  // Limpeza de audio da tela de inicio
+    al_destroy_bitmap(fundo);  // Limpeza da imagem de fundo
+    al_destroy_sample_instance(songInst); // Limpeza da Musica
+    return iniciar_jogo;
+}
